d338b575a383ba297344babc2d38fef398453170
Josh Stuible <joshstuible@gmail.com>
2020-08-25 16:17:12 -0700

    tighten up transitions between questions

--- pages/learn.vue
+++ pages/learn.vue
@@ -1,32 +1,42 @@
 <template>
   <main>
     <transition name="slide-fade" mode="out-in">
+      <h2 :key="currentIndex.section">{{currentSectionNumber}}/</h2>
+    </transition>
     <!-- Question View -->
-    <div class="question" v-if="!showFeedback" :key="currentIndex.question">
-      <h2>{{currentSectionNumber}}/</h2>
-      <question :question="currentQuestion.question" />
-      <learn-input :type="currentQuestion.type" :answers="currentQuestion.answers" :reveal-answer="showAnswer"/>
+    <transition name="slide-fade" mode="out-in">
+      <div class="question" v-if="!showFeedback" key="1">
+        <transition name="slide-fade" mode="out-in">
+          <div class="actual-question" :key="currentIndex.question">
+            <question :question="currentQuestion.question" />
+            <learn-input
+              :type="currentQuestion.type"
+              :answers="currentQuestion.answers"
+              :reveal-answer="showAnswer"
+            />
 
-      <vs-button flat :active="true" @click="showAnswer = true" v-if="!showAnswer" >Submit</vs-button>
-      <vs-button flat :active="true" @click="nextQuestion" v-else >Next</vs-button>
-    </div>
-    <!-- Feedback View -->
-    <div v-else>
-      <h2>Feedback</h2>
-      <feedback :feedback="currentSection.feedback" />
-      <vs-button
-        flat
-        :active="true"
-        @click="nextQuestion()"
-        class="submit"
-        v-if="!isLastSection"
-      >Next Section</vs-button>
-      <div v-else>DONE QUIZ</div>
-    </div>
+            <vs-button flat :active="true" @click="showAnswer = true" v-if="!showAnswer">Submit</vs-button>
+            <vs-button flat :active="true" @click="nextQuestion" v-else>Next</vs-button>
+          </div>
+        </transition>
+      </div>
+      <!-- Feedback View -->
+      <div v-else key="2">
+        <h2>Feedback</h2>
+        <feedback :feedback="currentSection.feedback" />
+        <vs-button
+          flat
+          :active="true"
+          @click="nextQuestion()"
+          class="submit"
+          v-if="!isLastSection"
+        >Next Section</vs-button>
+        <div v-else>DONE QUIZ</div>
+      </div>
+    </transition>
     <!-- DEBUG DATA -->
     <!-- <br />
-    {{currentSection}} -->
-    </transition>
+    {{currentSection}}-->
   </main>
 </template>
 
@@ -49,8 +59,6 @@ export default {
   },
   methods: {
     nextQuestion() {
-      // const this.currentSection.questions.length
-
       if (this.showFeedback) {
         this.currentIndex.question = 0;
         this.currentIndex.section++;
@@ -83,5 +91,6 @@ export default {
 <style>
 main {
   padding: 2em;
+  overflow: hidden;
 }
 </style>
