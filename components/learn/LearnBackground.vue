d7e7257993a90d138357913979db055cd07cd80d
Josh Stuible <joshstuible@gmail.com>
2020-09-09 14:28:04 -0700

    Use clipping mask instead of transformed div to achieve less bussy background

--- components/learn/LearnBackground.vue
+++ components/learn/LearnBackground.vue
@@ -1,46 +1,36 @@
 <template>
   <div class="learn-background-wrapper">
-      <div class="learn-background" :class="{accent: accent}"></div>
+    <div class="learn-background" :class="{accent: accent}"></div>
   </div>
 </template>
 
 <script>
 export default {
-    props: ["accent"]
-}
+  props: ["accent"],
+};
 </script>
 
 <style lang="scss">
 .learn-background-wrapper {
-    position: absolute;
-    height: 100%;
-    width: 100%;
-    right: 0;
-    top: 0;
-    overflow: hidden;
-    z-index: -1;
+  position: absolute;
+  overflow: hidden;
+  z-index: -1;
+  height: 100%;
+  width: 100%;
+  right: 0;
+  top: 0;
 }
 .learn-background {
-    position: relative;
-    right: -80%;
-    top: -20%;
-    transform: rotate(0deg);
-    height: 200vh;
-    width: 200vw;
-    background-color: $colour-primary;
+  position: relative;
+  background-color: $colour-primary;
+  height: 100%;
+  width: 120%;
+  right: 20%;
+  transition: clip-path 300ms ease-in-out;
+  clip-path: polygon(96% 0, 100% 0, 100% 100%, 85% 100%);
 
-    transition: all 200ms linear;
-
-    @include breakpoint(phone) {
-        transform: rotate(8deg);
-    }
-
-    @include breakpoint(phablet) {
-        transform: rotate(11deg);
-    }
-
-    &.accent {
-        right: 25%;
-    }
+  &.accent {
+    clip-path: polygon(11% 0, 100% 0, 100% 100%, 0% 100%);
+  }
 }
 </style>
