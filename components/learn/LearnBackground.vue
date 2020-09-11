2905d03fd5083dd20ded443d6120a2f964a936ba
Josh Stuible <joshstuible@gmail.com>
2020-09-11 13:26:47 -0700

    Use CSS Support Querries to provide fallback for browsers without clip-path support

--- components/learn/LearnBackground.vue
+++ components/learn/LearnBackground.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="learn-background-wrapper">
-    <div class="learn-background" :class="{accent: accent, legacy: !supportClipPath}" ref="learnBG"></div>
+    <div class="learn-background" :class="{accent: accent }" ref="learnBG"></div>
   </div>
 </template>
 
@@ -10,12 +10,7 @@ export default {
   data: () => ({
     supportClipPath: true,
   }),
-  mounted() {
-    // console.log(this.$refs);
-    this.$nextTick(() => {
-      this.supportClipPath = this.$refs.learnBG.style.clipPath === ""; //Detect support for clip-path
-    });
-  },
+  mounted() {},
 };
 </script>
 
@@ -31,24 +26,23 @@ export default {
 }
 .learn-background {
   position: relative;
-  background-color: $colour-primary;
+  background-color: $text-colour-light;
   height: 100%;
   width: 120%;
   right: 20%;
-  transition: clip-path 300ms ease-in-out;
-  clip-path: polygon(96% 0, 100% 0, 100% 100%, 85% 100%);
 
-  &.accent {
-    clip-path: polygon(11% 0, 100% 0, 100% 100%, 0% 100%);
+  transition: background-color 200ms ease-in-out;
+
+  @supports (clip-path: polygon(96% 0, 100% 0, 100% 100%, 85% 100%)) {
+    transition: clip-path 300ms ease-in-out;
+    background-color: $colour-primary;
+    clip-path: polygon(96% 0, 100% 0, 100% 100%, 85% 100%);
   }
 
-  // Don't use clip path, just change background colour if browser doesn't support clip-path
-  &.legacy {
-    background-color: $text-colour-light;
-    transition: background-color 200ms ease-in-out;
-
-    &.accent {
-      background-color: $colour-primary;
+  &.accent {
+    background-color: $colour-primary;
+    @supports (clip-path: polygon(96% 0, 100% 0, 100% 100%, 85% 100%)) {
+      clip-path: polygon(11% 0, 100% 0, 100% 100%, 0% 100%);
     }
   }
 }
