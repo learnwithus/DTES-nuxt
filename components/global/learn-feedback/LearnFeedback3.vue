06d6ad50a6858a196d19903c1ce2fc72ade1f3bb
Josh Stuible <joshstuible@gmail.com>
2020-09-14 14:35:54 -0700

    Create demo custom interactive component for section 3 feedback

---
+++ components/global/learn-feedback/LearnFeedback3.vue
@@ -0,0 +1,43 @@
+<template>
+  <markdown-container>
+    <markdown-column size="1" style="padding-right: 3em">{{contentToDisply ? content[contentToDisply] : "Hover over circles to view"}}</markdown-column>
+
+    <markdown-column size="1" style="padding-left: 3em">
+      <div class="cirlcle" @mouseover="contentToDisply = 'individual'">Individual Level</div>
+      <div class="cirlcle" @mouseover="contentToDisply = 'community'">Community Level</div>
+      <div class="cirlcle" @mouseover="contentToDisply = 'staff'">Staff Level</div>
+    </markdown-column>
+  </markdown-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    contentToDisply: false,
+    content: {
+      individual: 
+        `The negative impacts of stigma can be felt through the continuum of care, 
+        from individual stigma, to structural or institutional stigma, to broader stigma in society as a whole. 
+        Examples of these impacts can be seen by clicking on the levels of stigma:`,
+      community: "community",
+      staff: "staff",
+    },
+  }),
+};
+</script>
+
+<style scoped lang="scss">
+.cirlcle {
+  height: 6em;
+  width: 6em;
+  padding: 2em;
+  border: 2px white solid;
+  text-align: center;
+  border-radius: 50%;
+
+  &:hover{
+      background-color: white;
+      color: $colour-primary;
+  }
+}
+</style>
--- content/learn/section-3/feedback.md
+++ content/learn/section-3/feedback.md
@@ -1,14 +1,2 @@
 <!-- Section Level Feedback -->
-<markdown-container>
-  <markdown-column size="1">
-
-  Stigma can be defined as attitudes, behaviours, structures and/or beliefs that are prejudicial towards a group or individual based on life circumstance. 
-
-  The social process of stigma reinforces power and control roles and leads to discrimination, denial of services and loss of status for those that are stigmatized.
-
-  </markdown-column>
-  
-  <markdown-column size="1">
-    <markdown-image src="images/graph-placeholder.svg"></markdown-image>
-  </markdown-column>
-</markdown-container>
+<learn-feedback-3></learn-feedback-3>
