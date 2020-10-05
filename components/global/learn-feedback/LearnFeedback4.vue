371efaf2b72e34c203930a7a71ea72e1e46bca18
Josh Stuible <joshstuible@gmail.com>
2020-10-05 16:52:21 -0700

    Added full interactive content from storyboard to section 4 feedback

---
+++ components/global/learn-feedback/LearnFeedback4.vue
@@ -0,0 +1,135 @@
+<template>
+  <markdown-container>
+    <markdown-column size="1" style="padding-right: 3em">
+      <div v-if="sectionIndex == 0">
+        <h3>
+          20% of people overdosed after leaving Emergency Departments either
+          against medical advice
+        </h3>
+        <p>
+          Indicates there is a missed opportunity to engage these individuals in
+          care.
+        </p>
+        <p>
+          Potential strategies to engage this group could include harm reduction
+          initiatives and services at point of care, which in turn would
+          challenge stigma from healthcare workers.
+        </p>
+      </div>
+      <div v-else-if="sectionIndex == 1">
+        <h3>16% of people overdosed and had no engagement with medical care</h3>
+        <p>
+          Highlights a need for community-based / non-clinical strategies to
+          engage, focussing on reducing barriers to care
+        </p>
+      </div>
+      <div v-else-if="sectionIndex == 2">
+        <h3>
+          60% of individuals treated for illicit drug overdose in a hospital
+          setting did not have a mental health or substance use disorder
+          diagnosis in the year leading up to the overdose
+        </h3>
+        <ul>
+          <li>
+            Not all overdoses are characterised by use or mental health
+            diagnoses. This highlights the need for integration of harm
+            reduction and overdose education in routine daily care.
+          </li>
+          <li>
+            Stigma may result in under-diagnosis and reduced preventative
+            treatment
+          </li>
+        </ul>
+      </div>
+      <div v-else>
+        <p>
+          Study: BC healthcare utilization patterns by people who experience an
+          overdose from illegal drugs
+        </p>
+        <em>Click on the diagram on the right to learn more.</em>
+      </div>
+    </markdown-column>
+
+    <markdown-column size="1" style="padding-left: 3em">
+      <ul>
+        <li
+          v-for="(section, index) in sections"
+          v-bind:key="index"
+          class="utilization-section"
+          @mouseenter="setSection(index)"
+          :class="{ active: sectionIndex == index }"
+        >
+          <div class="percentage">{{ section.perecentage }}%</div>
+          <div class="right-section">
+            <img
+              :src="require(`~/assets/images/icons/${section.icon}`)"
+              alt=""
+            />
+            <p class="description">{{ section.description }}</p>
+          </div>
+        </li>
+      </ul>
+    </markdown-column>
+  </markdown-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    sectionIndex: undefined,
+    sections: [
+      {
+        perecentage: 20,
+        icon: "ambulance.svg",
+        description:
+          "of people overdosed after leaving Emergency Departments either against medical advice",
+      },
+      {
+        perecentage: 16,
+        icon: "hospital.svg",
+        description:
+          "of people overdosed and had no engagement with medical care",
+      },
+      {
+        perecentage: 60,
+        icon: "clipboard.svg",
+        description:
+          "of individuals treated for illicit drug overdose in a hospital setting did not have a mental health or substance use disorder diagnosis in the year leading up to the overdose",
+      },
+    ],
+  }),
+  methods: {
+    setSection(index) {
+      this.sectionIndex = index;
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.utilization-section {
+  display: flex;
+  align-items: center;
+  justify-content: left;
+  // margin-bottom: 2em;
+  padding: 1em;
+  border-radius: 1em;
+
+  &.active {
+    background-color: rgba($colour-dark, 0.5);
+  }
+
+  img {
+    height: 3em;
+  }
+}
+.right-section {
+  flex-grow: 1;
+}
+.percentage {
+  font-size: 2em;
+  padding-right: 2em;
+  // max-width: 10em;
+  // width: 10em;
+}
+</style>
--- content/learn/sections/section-4/feedback-2.md
+++ content/learn/sections/section-4/feedback-2.md
@@ -1,11 +1,2 @@
 <!-- Section Level Feedback -->
-<h2>Healthcare Example</h2>
-<markdown-container>
-  <markdown-column size="1"  style="padding-right: 3em">
-    <markdown-image src="images/graph-placeholder.svg"></markdown-image>
-  </markdown-column>
-  
-  <markdown-column size="1" style="padding-left: 3em">
-    <markdown-image src="images/graph-placeholder.svg"></markdown-image>
-  </markdown-column>
-</markdown-container>
+<learn-feedback-4></learn-feedback-4>
