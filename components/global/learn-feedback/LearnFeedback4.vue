977acc57c6c925b3c158fcd6694c172b48d4d069
Josh Stuible <joshstuible@gmail.com>
2020-10-06 13:15:21 -0700

    Added verticle slide fade transition for learn feedback pages, using click instead of mouseover

--- components/global/learn-feedback/LearnFeedback4.vue
+++ components/global/learn-feedback/LearnFeedback4.vue
@@ -1,53 +1,57 @@
 <template>
   <markdown-container>
     <markdown-column size="1" style="padding-right: 3em">
-      <div v-if="sectionIndex == 0">
-        <h3>
-          20% of people overdosed after leaving Emergency Departments either
-          against medical advice
-        </h3>
-        <p>
-          Indicates there is a missed opportunity to engage these individuals in
-          care.
-        </p>
-        <p>
-          Potential strategies to engage this group could include harm reduction
-          initiatives and services at point of care, which in turn would
-          challenge stigma from healthcare workers.
-        </p>
-      </div>
-      <div v-else-if="sectionIndex == 1">
-        <h3>16% of people overdosed and had no engagement with medical care</h3>
-        <p>
-          Highlights a need for community-based / non-clinical strategies to
-          engage, focussing on reducing barriers to care
-        </p>
-      </div>
-      <div v-else-if="sectionIndex == 2">
-        <h3>
-          60% of individuals treated for illicit drug overdose in a hospital
-          setting did not have a mental health or substance use disorder
-          diagnosis in the year leading up to the overdose
-        </h3>
-        <ul>
-          <li>
-            Not all overdoses are characterised by use or mental health
-            diagnoses. This highlights the need for integration of harm
-            reduction and overdose education in routine daily care.
-          </li>
-          <li>
-            Stigma may result in under-diagnosis and reduced preventative
-            treatment
-          </li>
-        </ul>
-      </div>
-      <div v-else>
-        <p>
-          Study: BC healthcare utilization patterns by people who experience an
-          overdose from illegal drugs
-        </p>
-        <em>Click on the diagram on the right to learn more.</em>
-      </div>
+      <transition name="verticle-slide-fade" mode="out-in">
+        <div v-if="sectionIndex == 0" key="0">
+          <h3>
+            20% of people overdosed after leaving Emergency Departments either
+            against medical advice
+          </h3>
+          <p>
+            Indicates there is a missed opportunity to engage these individuals
+            in care.
+          </p>
+          <p>
+            Potential strategies to engage this group could include harm
+            reduction initiatives and services at point of care, which in turn
+            would challenge stigma from healthcare workers.
+          </p>
+        </div>
+        <div v-else-if="sectionIndex == 1" key="1">
+          <h3>
+            16% of people overdosed and had no engagement with medical care
+          </h3>
+          <p>
+            Highlights a need for community-based / non-clinical strategies to
+            engage, focussing on reducing barriers to care
+          </p>
+        </div>
+        <div v-else-if="sectionIndex == 2" key="2">
+          <h3>
+            60% of individuals treated for illicit drug overdose in a hospital
+            setting did not have a mental health or substance use disorder
+            diagnosis in the year leading up to the overdose
+          </h3>
+          <ul>
+            <li>
+              Not all overdoses are characterised by use or mental health
+              diagnoses. This highlights the need for integration of harm
+              reduction and overdose education in routine daily care.
+            </li>
+            <li>
+              Stigma may result in under-diagnosis and reduced preventative
+              treatment
+            </li>
+          </ul>
+        </div>
+        <div v-else key="3">
+          <p>
+            Study: BC healthcare utilization patterns by people who experience
+            an overdose from illegal drugs
+          </p>
+          <em>Click on the diagram on the right to learn more.</em>
+        </div>
+      </transition>
     </markdown-column>
 
     <markdown-column size="1" style="padding-left: 3em">
@@ -56,7 +60,7 @@
           v-for="(section, index) in sections"
           v-bind:key="index"
           class="utilization-section"
-          @mouseenter="setSection(index)"
+          @mousedown="setSection(index)"
           :class="{ active: sectionIndex == index }"
         >
           <div class="percentage">{{ section.perecentage }}%</div>
@@ -111,12 +115,20 @@ export default {
   display: flex;
   align-items: center;
   justify-content: left;
+  cursor: pointer;
   // margin-bottom: 2em;
+  opacity: 0.25;
   padding: 1em;
   border-radius: 1em;
+  transition: opacity 200ms;
+
+  &:hover {
+    opacity: 0.5;
+  }
 
   &.active {
-    background-color: rgba($colour-dark, 0.5);
+    // background-color: rgba($colour-dark, 0.5);
+    opacity: 1;
   }
 
   img {
@@ -129,6 +141,9 @@ export default {
 .percentage {
   font-size: 2em;
   padding-right: 2em;
+  font-family: $font-serif;
+  font-weight: 200;
+  letter-spacing: 0.05em;
   // max-width: 10em;
   // width: 10em;
 }
