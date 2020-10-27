4b6491c0fa0c41976a7555c0cb193c0923c9d6e7
Josh Stuible <joshstuible@gmail.com>
2020-10-27 14:00:28 -0700

    make question text size responsive

--- components/learn/questions/LearnQuestion.vue
+++ components/learn/questions/LearnQuestion.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="learn-question">{{question}}</div>
+  <div class="learn-question">{{ question }}</div>
 </template>
 
 <script>
@@ -11,6 +11,18 @@ export default {
 <style lang="scss">
 .learn-question {
   font-family: $font-serif;
-  font-size: 1.5em;
+  font-size: 1.1em;
+
+  @include breakpoint(phone) {
+    font-size: 1.3em;
+  }
+
+  @include breakpoint(thone) {
+    font-size: 1.4em;
+  }
+
+  @include breakpoint(phablet) {
+    font-size: 1.5em;
+  }
 }
 </style>
