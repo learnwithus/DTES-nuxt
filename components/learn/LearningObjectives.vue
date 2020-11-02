22b19f44b2cee0aa37f28bf00c59d4ce24ab3efc
Josh Stuible <joshstuible@gmail.com>
2020-11-02 14:35:29 -0800

    Created learning objectives component

---
+++ components/learn/LearningObjectives.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="learning-objectives">
+    <h1 class="objectives-title">Learning Objectives</h1>
+    <nuxt-content :document="$props.learningObjectives" />
+  </div>
+</template>
+
+<script>
+export default {
+    props: ["learningObjectives"]
+};
+</script>
+
+<style lang="scss">
+.objectives-title {
+    font-family: $font-serif;
+}
+</style>
