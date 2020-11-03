975e899d41d557861f09cceaf2d022d4c97b596d
Josh Stuible <joshstuible@gmail.com>
2020-11-02 22:18:45 -0800

    wrap objectives in 10 of 12 column so it doens't clash with background

--- components/learn/LearningObjectives.vue
+++ components/learn/LearningObjectives.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="learning-objectives">
     <h1 class="objectives-title">Learning Objectives</h1>
-    <nuxt-content :document="$props.learningObjectives" />
+    <div class="columns">
+      <nuxt-content :document="$props.learningObjectives" class="column is-10"/>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    props: ["learningObjectives"]
+  props: ["learningObjectives"],
 };
 </script>
 
 <style lang="scss">
 .objectives-title {
-    font-family: $font-serif;
+  font-family: $font-serif;
 }
 </style>
