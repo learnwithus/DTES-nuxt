c38bfa39460e07e36c8e3968618f574503c0c8d2
Josh Stuible <joshstuible@gmail.com>
2020-11-18 14:47:25 -0800

    Inlcude required vsOption element

--- plugins/vuesax.js
+++ plugins/vuesax.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import vsButton from 'vuesax/dist/vsButton'
 import vsSelect from 'vuesax/dist/vsSelect'
 import vsRadio from 'vuesax/dist/vsRadio'
+import vsOption from 'vuesax/dist/vsOption'
 
 //Vuesax styles
 import 'vuesax/dist/vuesax.css'
@@ -10,3 +11,4 @@ import 'vuesax/dist/vuesax.css'
 Vue.use(vsButton)
 Vue.use(vsSelect)
 Vue.use(vsRadio)
+Vue.use(vsOption)
