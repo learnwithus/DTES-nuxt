7dc77a3320f59fec9aefa3781a5a0947b316e335
Josh Stuible <joshstuible@gmail.com>
2020-11-30 12:50:06 -0800

    Added content to about

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
-    <div class="container"><h1>About</h1></div>
+    <div class="container">
+      <h1>About</h1>
+      <p>
+        The community of the DTES is multi-cultural, with many residents
+        connected to culturally diverse communities and histories, including the
+        Chinatown district, as well the histories of Japanese residents in
+        Oppenheimer, prior to World War II and the devastation of the internment
+        camps. While the community is located on Coast Salish territories, the
+        DTES has a higher proportion of Indigenous people than other communities
+        within Vancouver, and includes Indigenous people from across North
+        America. While recognized for poverty, homelessness, substance use, the
+        DTES also needs to be acknowledged for their multi-cultural,
+        inter-connected communities that care for each other.
+      </p>
+    </div>
   </div>
 </template>
 
