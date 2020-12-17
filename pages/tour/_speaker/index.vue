407de8a0bc5687326eb0e517c6f7933dda53fbe9
Josh Stuible <joshstuible@gmail.com>
2020-12-16 18:37:55 -0800

    add hidden nuxt-link so that video pages for speakers are generated (temp hack)

--- pages/tour/_speaker/index.vue
+++ pages/tour/_speaker/index.vue
@@ -19,6 +19,10 @@
             class="watch-button"
             >Watch</vs-button
           >
+          <nuxt-link
+            :to="`/tour/${speaker.slug}/watch`"
+            style="display: none"
+          />
         </div>
         <tour-minimap class="speaker-map" :location="speaker.slug" />
       </div>
