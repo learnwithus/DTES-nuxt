ec682a5b885d8f36366f4ec433f05f1325fdaf55
Josh Stuible <joshstuible@gmail.com>
2020-12-23 15:42:38 -0800

    broke up paragraph

--- pages/tour/intro.vue
+++ pages/tour/intro.vue
@@ -17,13 +17,15 @@
           connected to culturally diverse communities and histories, including
           the Chinatown district, as well the histories of Japanese residents in
           Oppenheimer, prior to World War II and the devastation of the
-          internment camps. While the community is located on Coast Salish
-          territories, the DTES has a higher proportion of Indigenous people
-          than other communities within Vancouver, and includes Indigenous
-          people from across North America. While recognized for poverty,
-          homelessness, substance use, the DTES also needs to be acknowledged
-          for their multi-cultural, inter-connected communities thatcare for
-          each other.
+          internment camps.
+        </p>
+        <p>
+          While the community is located on Coast Salish territories, the DTES
+          has a higher proportion of Indigenous people than other communities
+          within Vancouver, and includes Indigenous people from across North
+          America. While recognized for poverty, homelessness, substance use,
+          the DTES also needs to be acknowledged for their multi-cultural,
+          inter-connected communities thatcare for each other.
         </p>
       </div>
       <div class="column">
