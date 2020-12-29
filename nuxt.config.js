cd84b14b42df8c57a79c5490e088c01b7640e127
Josh Stuible <joshstuible@gmail.com>
2020-12-29 12:24:31 -0800

    Add favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,10 @@ export default {
       },
     ],
     link: [
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      { rel: "apple-touch-icon", type: "image/x-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+      { rel: "icon", type: "image/x-icon", sizes: "32x32", href: "/favicon-32x32.png" },
+      { rel: "icon", type: "image/x-icon", sizes: "16x16", href: "/favicon-16x16.png" },
+      { rel: "manifest", type: "image/x-icon", href: "/site.webmanifest" },
       { rel: "stylesheet", href: "https://use.typekit.net/wgf8sbs.css" },
     ],
     script: [
