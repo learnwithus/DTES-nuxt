5ac8e3d1c06bcbe4c9a743751355c9f18c046c4d
Josh Stuible <joshstuible@gmail.com>
2020-12-29 16:22:10 -0800

    spelling

--- pages/unsupported-browser.vue
+++ pages/unsupported-browser.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="container mt-6">
-    <h1 class="light-page-title">Inernet Explorer Not Supported</h1>
+    <h1 class="light-page-title">Internet Explorer Not Supported</h1>
     <p>
-      This website an interactive, media-heavy website that was designed for
+      This website is an interactive, media-heavy experience that was designed for
       modern browsers. Please use a browser such as Firefox or Chrome to avoid
       errors while navigating the site.
     </p>
